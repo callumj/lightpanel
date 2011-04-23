d314be53b0759d34e98a2070f3cc6a5dec84c338
callumj <callum@callumj.com>
2011-04-23 18:00:35 +0800

    Added support on iOS app to invoke actions (strobe, scroll) over web app

---
+++ ios/LightPanel/ActionsPanelViewController.h
@@ -0,0 +1,16 @@
+//
+//  ActionsPanelViewController.h
+//  LightPanel
+//
+//  Created by Callum Jones on 22/04/11.
+//  Copyright 2011 mullac.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ActionsPanelViewController : UIViewController {
+    
+}
+
+@end
